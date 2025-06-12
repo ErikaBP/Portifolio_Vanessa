1c2c18267a0a714b5cc095fd1d2de697ab30c2a2
ErikaBP <erikaborgessp@gmail.com>
2025-06-12 00:27:04 -0300

    Adicionando seção sinestesia com vídeo

--- portifolio_vanessa/src/pages/Home/page.tsx
+++ portifolio_vanessa/src/pages/Home/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './page.css';
 import Header from '../../components/header/Header';
+import { Link } from 'react-router-dom';
 const HomePage = () => {
     return (
       <div className="context">
@@ -22,6 +23,15 @@ const HomePage = () => {
           </div>
           <div className='sinestesia'>
             <img src="./images/sinestesiaIcon.svg" alt="Sinestesia" />
+          </div>
+          <div className='contextSinestesia'>
+            <video width="640" height="360" controls style={{ borderRadius: '15px' }}><source src="./videos/sinestesia.mp4" type="video/mp4" /></video>
+            <div className='textSinestesia'>
+                <p className='pSobreSinestesia'><b> SYNESTHESIA </b> é um projeto de experiência sinestésica que busca simular a condição por meio de uma <b> instalação </b> . O objetivo é capturar essa sinergia sensorial em uma <b> experiência visual </b> e <b> auditiva </b>. A instalação é projetada para receber <b> estímulos sonoros </b> do ambiente e convertê-los em <b> cores de luzes </b> , criando uma representação visual dos sons que são ouvidos, e proporcionando uma <b> experiência sensorial imersiva </b> .</p>
+                <div className='estiloVerMais'>
+                    <Link to='https://drive.google.com/drive/folders/1S2SxhbyLOrk2ccgRvW-0eErXNiwc-m8d' className='verMais'>Ver mais</Link>
+                </div>
+            </div>
           </div>
         </div>
       </div>
