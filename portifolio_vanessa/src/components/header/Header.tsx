d7330a496180c08f41c8d92ce7a68764115d8627
ErikaBP <erikaborgessp@gmail.com>
2025-06-11 22:45:13 -0300

    Estrutura inicial e header

---
+++ portifolio_vanessa/src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+  
+  function Header() {
+    return (
+      <div className="menu">
+        <div className="menuItens">
+          <h1 className='h1Header'>Sobre</h1>
+          <h1 className='h1Header'>Projetos</h1>
+          <Link to="" className="linkHeader"> 
+            <h1 className='h1Header'>Havaianas</h1>
+          </Link>
+          <Link to="" className="linkHeader"> 
+            <h1 className='h1Header'>Girafa</h1>
+          </Link>
+        </div>
+      </div>
+    );
+  };
+
+export default Header;
---
+++ portifolio_vanessa/src/pages/Home/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './page.css';
+import Header from '../../components/header/Header';
+const HomePage = () => {
+    return (
+      <div className="context">
+        <Header />
+        <div className="container">
+          
+        </div>
+      </div>
+    );
+  };
+  
+  export default HomePage;
